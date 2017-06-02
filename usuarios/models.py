41a349907a401b522adabc2c757a6b9bc0124ce0
Yojan Pardo <yojan.pardo@gmail.com>
2017-06-02 10:10:55 +0000

    agrego mas cositas a gitignore y añado contraseñas a usuarios/models.py

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Usuario(models.Model):
     name = models.CharField(max_length=15, unique=True)
     password = models.CharField(max_length=30)
+    password2 = models.CharField(max_length=30)
     rol = models.CharField(max_length=30)
     phone = models.CharField(max_length=11)
     email = models.CharField(max_length=30)
